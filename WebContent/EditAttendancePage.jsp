e3729148af0fa2ee67a5c2e271aa89b8f35de128
koonchaim <koonchaim@gmail.com>
2014-09-17 13:52:18 +0700

    Commit Project Whit koonchaim

---
+++ WebContent/EditAttendancePage.jsp
@@ -0,0 +1,39 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+	<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
+				<!-- /.Search Education -->
+				<div class="row form-group" align="center">
+					<form class="form-inline" role="form">
+						<div class="form-group">
+							<input class="form-control" id="DateShow" type="text" disabled>
+						</div>
+						<div class="form-group">
+							<input class="form-control" id="EducationShow" type="text" disabled>
+						</div>
+						<div class="form-group">
+							<input class="form-control" id="TermShow" type="text"  disabled>
+						</div>
+					</form>
+				</div>
+
+				<div class="table-responsive">
+					<form>
+						<table id="modal-table" class="table table-bordered table-hover">
+							<thead>
+								<tr>
+									<th>ลำดับ</th>
+									<th>รหัสประจำตัวนักศึกษา</th>
+									<th>ชื่อ</th>
+									<th>นามสกุล</th>
+									<th>ผลการเช็คชื่อ</th>
+								</tr>
+							</thead>
+							<tbody id="tbody-show">
+	
+							</tbody>
+						</table>
+
+					</form>
+				</div>
+				<!-- /.box-body -->
---
+++ src/com/project/edit_attendance/EditAttendanceServlet.java
@@ -0,0 +1,88 @@
+package com.project.edit_attendance;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.project.bean.ScheduleBean;
+
+/**
+ * Servlet implementation class EditAttendanceServlet
+ */
+@WebServlet("/EditAttendanceServlet")
+public class EditAttendanceServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public EditAttendanceServlet() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+
+		EditAttendanceManager edtMng = new EditAttendanceManager();
+		if (request.getParameter("studentID") == null) {
+			String majorName = request.getParameter("majorName");
+			String eduBackground = request.getParameter("eduBackground");
+			String eduLevel = request.getParameter("eduLevel");
+			int intEduLevel = Integer.parseInt(eduLevel);
+			String term = request.getParameter("term");
+			String dateAttendance = request.getParameter("dateAttendance");
+
+			/*
+			 * Show Date
+			 */
+
+			try {
+				Date result = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH).parse(dateAttendance);
+				System.out.println(result);
+				List<ScheduleBean> listSchedule = edtMng.listStudentAndSchedule(majorName, eduBackground, intEduLevel, term, result);
+				request.getSession().setAttribute("listSchedule", listSchedule);
+				for (int i = 0; i < listSchedule.size(); i++) {
+					System.out.println(listSchedule.get(i).getAttendance().getStudent().getStudentID()+"  -  "+listSchedule.get(i).getAttendance().getStatusActivity());
+				}
+				String json1 = new Gson().toJson(listSchedule);
+				response.setContentType("application/json");
+				response.setCharacterEncoding("UTF-8");
+				response.getWriter().write(json1);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+}
