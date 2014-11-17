3897b6abb71e75ba7d7b29d983e8c707c56747e0
koonchaim <koonchaim@gmail.com>
2014-11-17 12:20:30 +0700

    Commit 3

--- src/com/project/utility/ConnectDB.java
+++ src/com/project/utility/ConnectDB.java
@@ -7,13 +7,8 @@ public class ConnectDB {
 	private static ConnectDB instance = null;
 	private static String DB_DRIVER = "com.mysql.jdbc.Driver";
 	private static String DB_CONNECTION = "jdbc:mysql://localhost:3306/proj2014_013?characterEncoding=utf-8";
-<<<<<<< HEAD
 	private static String DB_USER = "proj2014_013";
 	private static String DB_PASSWORD = "proj2014_013";
-=======
-	private static String DB_USER = "root";
-	private static String DB_PASSWORD = "1234";
->>>>>>> d3e4f8d5608e9d1716edb7fb102ac923243c6734
 
 	public Connection DBConnection() {
 		Connection dbConnection = null;
